afedbb9d6d01526253aabaada7b8c404c609fc50
timsmeets23 <33163856+timsmeets23@users.noreply.github.com>
2017-11-29 21:16:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'delta') {
-    	message.reply('Are You Ready Kids OOOOOOH Who ends in Pineapple and shit talks constantly DELTA PINEAPPLE He is trashy and selfish and annoying (You see?) DELTA PINEAPPLE He has a big ego and acts like a bitch DELTA PINEAPPLE He is nothing but drama and throws tons of fits! READY DELTA PINEAPPLE DELTA PINEAPPLE DELTA .. SHUT UP...... NO ONE LIKES YOU, YOU CAN LEAVE, GOODBYE!');
+    	message.reply('Are You Ready Kids OOOOOOH Who ends in Pineapple and shit talks constantly DELTA PINEAPPLE He`s trashy and selfish and annoying (You see?) DELTA PINEAPPLE He has a big ego and acts like a bitch DELTA PINEAPPLE He`s nothing but drama and throws tons of fits! READY DELTA PINEAPPLE DELTA PINEAPPLE DELTA .. SHUT UP...... NO ONE LIKES YOU, YOU CAN LEAVE, GOODBYE!');
   	}
 });
 
