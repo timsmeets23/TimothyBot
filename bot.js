2159d794d5f2b045fc47e510a1a9f4add0895615
timsmeets23 <33163856+timsmeets23@users.noreply.github.com>
2017-12-12 17:32:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,19 +6,22 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'hello') {
+    if (message.content === '-hello') {
     	message.reply('OH HELLLOWWWWW...');
     }
-    if (message.content === ':0') {
+    if (message.content === '-:o') {
         message.reply('Oh.. no.. no.. honey.. honey.. no');
     }
-    if (message.content === 'matt') {
+    if (message.content === '-matt') {
         message.reply('What..');
     }
-    if (message.content === 'peridot') {
+    if (message.content === '-peridot') {
         message.channel.send("What's the point of corn....", {
         file: "https://i.pinimg.com/originals/31/75/0f/31750ffc2bae0e98a3fbe4a172be621a.jpg" // Or replace with FileOptions object
         });
+    }
+    if (message.content === '-commandlist') {
+        message.reply('-hello,-:o,-matt,-peridot,-commandlist')
     }
 });
 
